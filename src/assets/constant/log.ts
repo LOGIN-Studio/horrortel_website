bd81f6177bf82d6e1fa37339e3ddddb283b53b58
loginFN <166138786+loginFN@users.noreply.github.com>
2024-05-04 17:34:55 +0800

    Update log.ts for lzh-1
    log update of lzh -1

--- src/assets/constant/log.ts
+++ src/assets/constant/log.ts
@@ -1,6 +1,14 @@
 import { LogType } from './log_type';
 
 export const projectLog: LogType[] = [
+    {
+    date: '2024-04-02',
+    text: `Completed game design and some player operations
+1. Initial game design proposal
+2. Simple click button to achieve movement
+3. Conceptual design of UI`,
+    images: ['lzh-log-1.png', 'lzh-log-2.png', 'lzh-log-3.png','lzh-log-4.png'],
+  },
   {
     date: '2024-04-01',
     text: `Completion of the project's official website, which includes a display of the project's progress.
